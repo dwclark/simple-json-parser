45a7d6f295b79077ba09e5833187dd47eb81383b
David Clark <me@nobody.com>
2025-06-04 20:11:38 -0500

    Made good progress on event based decoder

--- src/decode-event.lisp
+++ src/decode-event.lisp
@@ -1,46 +1,50 @@
 (in-package :simple-json-parser)
 
-(deftype event (s)
-  (member s '(:string :escaped-string :integer :float :true :false :null
-	      :start-object :end-object :end-key :start-array :end-array :end-item :eof)))
+(declaim (optimize (speed 0) (debug 3)))
 
-(defstruct (parser (:conc-name ""))
-  (read-buffer "" :type simple-string)
-  (pos -1 :type fixnum)
-  (eof nil :type boolean))
+(defgeneric current (parser))
+(defgeneric next (parser))
+(defgeneric prev (parser))
+(defgeneric peek (parser))
 
-(defun next (my-parser)
-  (declare (type parser my-parser))
-  (if (< (incf (pos my-parser)) (length (read-buffer my-parser)))
-      (aref (read-buffer my-parser) (pos my-parser))
-      #\Nul))
+(defclass simple-string-parser ()
+  ((read-buffer :initarg :read-buffer :initform (error "must supply read buffer") :reader read-buffer)
+   (pos :initarg :pos :initform -1 :reader pos)))
 
-(defun current (my-parser)
-  (aref (read-buffer my-parser) (pos my-parser)))
+(defmethod next ((parser simple-string-parser))
+  (with-slots (read-buffer pos) parser
+    (if (< (incf pos) (length read-buffer))
+	(aref read-buffer pos)
+	#\Nul)))
 
-(defun prev (my-parser)
-  (declare (type parser my-parser))
-  (aref (read-buffer my-parser) (1- (pos my-parser))))
+(defmethod current ((parser simple-string-parser))
+  (with-slots (read-buffer pos) parser
+    (if (< pos (length read-buffer))
+	(aref read-buffer pos)
+	#\Nul)))
 
-(defun peek (my-parser)
-  (declare (type parser my-parser))
-  (if (< (1+ (pos my-parser)) (length (read-buffer my-parser)))
-      (aref (read-buffer my-parser) (1+ pos))
-      #\Nul))
+(defmethod prev ((parser simple-string-parser))
+  (with-slots (read-buffer pos) parser
+    (aref read-buffer (1- pos))))
+
+(defmethod peek ((parser simple-string-parser))
+  (with-slots (read-buffer pos) parser
+    (if (< (1+ pos) (length read-buffer))
+	(aref read-buffer (1+ pos))
+	#\Nul)))
 
 (defun whitespace-p (c)
-  (case (next my-parser)
+  (case c
     ((#\Space #\Tab #\Return #\Linefeed) t)
     (otherwise nil)))
 
 (defun consume-number (start my-parser)
-  (declare (type parser my-parser))
-
   (loop while (digit-char-p (peek my-parser))
 	do (next my-parser))
 
-  (when (whitespace-p (peek my-parser))
-    (return-from consume-number (values :integer start (pos my-parser))))
+  (let ((c (peek my-parser)))
+    (when (or (whitespace-p c) (char= c #\Nul))
+      (return-from consume-number (values :integer start (pos my-parser)))))
 
   (when (char= #\. (peek my-parser))
     (next my-parser)
@@ -62,9 +66,8 @@
   (values :float start (pos my-parser)))
 
 (defun consume-string (start my-parser)
-  (declare (type parser my-parser))
   (loop with event-type = :string
-	for c = (next) then (next)
+	for c = (next my-parser) then (next my-parser)
 	until (and (char= #\" c) (not (char= #\\ (prev my-parser))))
 	do (if (char= #\\ c)
 	       (case (peek my-parser)
@@ -73,24 +76,22 @@
 	finally (return (values event-type start (pos my-parser)))))
 
 (defun consume-whitespace (my-parser)
-  (declare (type parser my-parser))
-  (loop while (whitespace-p my-parser)))
+  (loop while (whitespace-p (next my-parser))))
 
 (defun consume-exact (my-parser expect)
-  (declare (type parser my-parser))
-  (declare (type simple-string expect))
   (loop for c across expect
-	do (if (not (char= c (next my-parser)))
+	do (if (char= c (peek my-parser))
+	       (next my-parser)
 	       (error (format t "expected ~A" c)))
 	finally (return (pos my-parser))))
 
 ;; :string :escaped-string :integer :float :true :false :null
 ;; :start-object :end-object :start-array :end-array :end-key :end-item :eof
 (defun next-event (my-parser)
-  (declare (type parser my-parser))
-  (consume-whitespace)
+  (consume-whitespace my-parser)
   (let ((start (pos my-parser)))
-    (case (next my-parser)
+    (format t "next-event: start: ~A, current: ~A~%" start (current my-parser))
+    (case (current my-parser)
       (#\t (values :true start (consume-exact my-parser "rue")))
       (#\f (values :false start (consume-exact my-parser "alse")))
       (#\n (values :null start (consume-exact my-parser "ull")))
@@ -104,79 +105,119 @@
       (#\] (values :end-array start start))
       (#\Nul (values :eof -1 -1)))))
 
-(defun decode-event (src)
-  (let ((stack (make-array 1024))
-	(stack-loc -1)
-	(nesting -1)
-	(my-parser (etypecase (src)
-		     (simple-string (make-parser :read-buffer src)))))
-    (declare (type parser my-parser))
+(defstruct json-stack
+  (contents (make-array 1024) :type (simple-vector 1024))
+  (location -1 :type fixnum))
 
-    (labels ((pop-stack ()
-	       (let ((ret (aref stack stack-loc)))
-		 (decf stack-loc)
-		 ret))
-	     (push-stack (item)
-	       (setf (aref stack (incf stack-loc)) item))
-	     (pop-to-container ()
-	       (if (typep (aref stack (1- stack-loc)) 'cons)
-		   (let ((value (pop-stack))
-			 (ary (cdr (aref stack stack-loc))))
-		     (vector-push-extend value))
-		   (let ((value (pop-stack))
-			 (key (pop-stack))
-			 (table (cdr (aref stack stack-loc))))
-		     (setf (gethash key table) value))))
-	     (pop-guard-empty ()
-	       (let ((top (aref stack stack-loc)))
-		 (if (not (and (typep top 'cons)
-			       (= (car top) nesting)))
-		     (pop-to-container)))))
-      
-      (loop do (multiple-value-bind (evt start end) (next-event my-parser)
-		 (declare (type evt event))
-		 (declare (type fixnum start end))
-		 (etypecase (evt)
-		   (:true (push-stack :true))
-		   (:false (push-stack :false))
-		   (:null (push-stack :null))
-		   (:integer (push-stack (parse-integer (read-buffer my-parser) :start start :end (1+ end))))
-		   (:float (push-stack (read-from-string (read-buffer my-parser) :start start :end (1+ end))))
-		   ((:string :escaped-string) ;;TODO: need to escape string if necessary
-		    (loop with s = (1+ start)
-			  with ary = (make-string (- end s))
-			  loop for i from s below end
-			  do (setf (aref ary (- i s)) (aref (read-buffer my-parser) i))
-			  finally (push-stack ary)))
-		   
-		   (:start-object
-		    (setf (aref stack (incf stack-loc))
-			  (cons (incf nesting) (make-hash-table :test #'equal))))
-		   
-		   (:end-key
-		    (if (not (typep (aref stack stack-loc) 'string))
-			(error "expecting string type as key")))
+(defun json-stack-push (stack item)
+  (declare (type json-stack stack))
+  (setf (aref (json-stack-contents stack) (incf (json-stack-location stack))) item))
 
-		   (:end-item
-		    (pop-to-container))
-		   
-		   (:end-object
-		    (pop-guard-empty)
-		    (decf nesting))
-		   
-		   (:start-array
-		    (setf (aref stack (incf stack-loc))
-			  (cons (incf nesting) (make-array 0 :adjustable t :fill-pointer 0))))
-		    
-		   (:end-array
-		    (pop-guard-empty)
-		    (decf nesting))
-		   
-		   (:eof
-		    (if (zerop stack-loc)
-			(aref stack stack-loc)
-			(error "unexpected eof"))))))))
+(defun json-stack-at (stack i &optional (item nil item-supplied-p))
+  (declare (type json-stack stack))
+  (declare (type fixnum i))
+  (if item-supplied-p
+      (setf (aref (json-stack-contents stack) (+ i (json-stack-location stack))) item)
+      (aref (json-stack-contents stack) (+ i (json-stack-location stack)))))
 
+(defun json-stack-pop (stack)
+  (declare (type json-stack stack))
+  (let ((ret (json-stack-at stack 0)))
+    (json-stack-at stack 0 nil)
+    (decf (json-stack-location stack))
+    ret))
+
+(defun json-stack-push-array (stack)
+  (declare (type json-stack stack))
+  (let* ((new-array (make-array 0 :adjustable t :fill-pointer 0))
+	 (cell (cons new-array nil)))
+    (json-stack-push stack cell)))
+
+(defun json-stack-push-object (stack)
+  (declare (type json-stack stack))
+  (let* ((new-object (make-hash-table :test #'equal))
+	 (cell (cons new-object nil)))
+    (json-stack-push stack cell)))
+
+(defun json-stack-pop-item (stack)
+  (declare (type json-stack stack))
+  (let ((top (json-stack-pop stack))
+	(array-cell (json-stack-at stack 0)))
+    (if (consp array-cell)
+	(if (typep (car array-cell) 'array)
+	    (let ((array (car array-cell)))
+	      (vector-push-extend top array)
+	      (return-from json-stack-pop-item))
+	    (error "expected array element, found something else")))
     
-  
-  
+    (let ((value top)
+	  (key (json-stack-pop stack))
+	  (table-cell (json-stack-at stack 0)))
+      (if (consp table-cell)
+	  (if (typep (car table-cell) 'hash-table)
+	      (let ((table (car table-cell)))
+		(setf (gethash key table) value))
+	      (error "expected object element, found something else"))))))
+
+(defun json-stack-pop-array (stack)
+  (declare (type json-stack stack))
+  (let ((top-item (json-stack-at stack 0)))
+    (if (atom top-item)
+	(json-stack-pop-item stack))
+    (json-stack-at stack 0 (car (json-stack-at stack 0)))))
+
+(defun json-stack-pop-object (stack)
+  (declare (type json-stack stack))
+  (let ((top-item (json-stack-at stack 0)))
+    (if (atom top-item)
+	(json-stack-pop-item stack))
+    (json-stack-at stack 0 (car (json-stack-at stack 0)))))
+
+(defun json-stack-validate-key (stack)
+  (declare (type json-stack stack))
+  (if (not (typep (json-stack-at stack 0) 'string))
+      (error "invalid key type")))
+
+(defun decode-event (src)
+  (let ((stack (make-json-stack))
+	(my-parser (etypecase src
+		     (simple-string (make-instance 'simple-string-parser :read-buffer src)))))
+    
+    (loop do (multiple-value-bind (evt start end) (next-event my-parser)
+	       (format t "evt: ~A, start: ~A, end: ~A~%" evt start end)
+	       (ecase evt
+		 (:true (json-stack-push stack :true))
+		 (:false (json-stack-push stack :false))
+		 (:null (json-stack-push stack :null))
+		 (:integer
+		  (json-stack-push stack (parse-integer (read-buffer my-parser) :start start :end (1+ end))))
+		 
+		 (:float
+		  (let ((*read-default-float-format* 'double-float))
+		    (json-stack-push stack (read-from-string (read-buffer my-parser) t #\Nul :start start :end (1+ end)))))
+		 
+		 ((:string :escaped-string) ;;TODO: need to escape string if necessary
+		  (json-stack-push stack (subseq (read-buffer my-parser) (1+ start) end)))
+		 
+		 (:start-object
+		  (json-stack-push-object stack))
+		  
+		 (:end-key
+		  (json-stack-validate-key stack))
+		 
+		 (:end-item
+		  (json-stack-pop-item stack))
+		 
+		 (:end-object
+		  (json-stack-pop-object stack))
+		  
+		 (:start-array
+		  (json-stack-push-array stack))
+		 
+		 (:end-array
+		  (json-stack-pop-array stack))
+		 
+		 (:eof
+		  (if (zerop (json-stack-location stack))
+		      (return (json-stack-pop stack))
+		      (error "unexpected eof"))))))))
